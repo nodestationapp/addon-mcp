890ea1d5ffcbd14fa5b47d9056642d7567244856
Krystian <krystian.wachnicki5@gmail.com>
2025-07-19 18:26:05 +0700

    fix: change documentation directory

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
 #!/usr/bin/env node
 
 import fs from "fs";
-import path from "path";
-import { runCommand } from "@nstation/utils";
+import dotenv from "dotenv";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { runCommand, rootPath } from "@nstation/utils";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 if (process.argv[2] === "generate") {
-  console.log(process.env);
+  const baseUrl = process.env?.PUBLIC_URL;
+
   try {
-    const mcpFolderDir = path.join("./", "mcp-server");
+    let swaggerPath;
+
+    const nodeModulesPath = path.join(
+      rootPath,
+      "node_modules",
+      "@nstation",
+      "addon-docs",
+      "server",
+      "swagger.json"
+    );
+
+    const pluginsPath = path.join(
+      rootPath,
+      "plugins",
+      "docs",
+      "server",
+      "swagger.json"
+    );
+
+    if (fs.existsSync(nodeModulesPath)) {
+      swaggerPath = nodeModulesPath;
+    } else if (fs.existsSync(pluginsPath)) {
+      swaggerPath = pluginsPath;
+    } else {
+      throw new Error("Documentation plugin not found");
+    }
+
+    const mcpFolderDir = path.join(__dirname, "mcp-server");
 
     if (fs.existsSync(mcpFolderDir)) {
       fs.rmSync(mcpFolderDir, { recursive: true });
     }
 
     await runCommand({
-      cmd: "npm",
-      args: ["run", "generate-mcp"],
+      cmd: "./node_modules/.bin/openapi-mcp-generator",
+      __dirname: __dirname,
+      args: [
+        "--input",
+        swaggerPath,
+        "--output",
+        "./mcp-server",
+        "--base-url",
+        baseUrl,
+      ],
     });
 
     await runCommand({
